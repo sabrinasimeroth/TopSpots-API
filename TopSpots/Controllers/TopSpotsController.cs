b630ca7afb4ebef39e4a40932c8c3b98ccc2ea08
Sabrina Simeroth <Sabrina Simeroth>
2017-03-29 11:57:15 -0700

    Initial Commit

---
+++ TopSpots/Controllers/TopSpotsController.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using System.IO;
+using TopSpots.Models;
+using Newtonsoft.Json;
+
+namespace TopSpots.Controllers
+{
+    public class TopSpotsController : ApiController
+    {
+		[HttpGet, Route("api/topspots")]
+
+		public IHttpActionResult GetAllTopSpots()
+		{
+			string TopSpots = File.ReadAllText("C:\\dev\\TopSpots\\TopSpots.json");
+
+			//convert string above into an array of c# objects 
+			TopSpot[] topSpotsArray = JsonConvert.DeserializeObject<TopSpot[]>(TopSpots);
+
+			return Ok(topSpotsArray);
+		}
+	}
+}
---
+++ TopSpots/Models/TopSpot.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace TopSpots.Models
+{
+	public class TopSpot
+	{
+		public string Name { get; set; }
+
+		public double[] Location { get; set; }
+
+		public string Description { get; set; }
+	}
+}
